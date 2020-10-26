1793037d224fb91d5bc3184549009b6593bb4fad
Kyle Trahan <ktrahan2@gmail.com>
2020-10-26 17:36:21 -0400

    removed age from users table

--- models/user.go
+++ models/user.go
@@ -8,7 +8,6 @@ type User struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 	Email    string `json:"email"`
-	Age      int    `json:"age"`
 }
 
 //Users is an exported type that contains the instance of a User
