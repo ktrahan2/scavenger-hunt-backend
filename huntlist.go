4078be68255910f3b125dd88245a2db31590435a
Kyle Trahan <ktrahan2@gmail.com>
2020-11-08 12:31:17 -0500

    remove *

--- huntlist.go
+++ huntlist.go
@@ -12,9 +12,9 @@ import (
 //HuntList is the list created randomly and saved to a user
 type HuntList struct {
 	gorm.Model
-	Title   string  `json:"title"`
-	OwnerID uint    `json:"ownerid"`
-	Users   []*User `gorm:"many2many:user_list;"`
+	Title   string `json:"title"`
+	OwnerID uint   `json:"ownerid"`
+	Users   []User `gorm:"many2many:user_list;"`
 }
 
 //allHuntLists selects * from hunt_lists
