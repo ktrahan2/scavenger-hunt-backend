f91dc18a269193e844900c3e78fae1c63d835e84
Kyle Trahan <ktrahan2@gmail.com>
2020-11-08 12:34:09 -0500

    so close

--- huntlist.go
+++ huntlist.go
@@ -14,7 +14,7 @@ type HuntList struct {
 	gorm.Model
 	Title   string `json:"title"`
 	OwnerID uint   `json:"ownerid"`
-	UserID  uint   `gorm:"many2many:user_list;"`
+	Users   []User `gorm:"many2many:user_list;"`
 }
 
 //allHuntLists selects * from hunt_lists
